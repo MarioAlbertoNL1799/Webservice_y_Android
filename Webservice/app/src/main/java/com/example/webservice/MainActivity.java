1c3d92a847020d1e0ded1fb139c6684edb828146
MarioAlbertoNL1799 <1717110635@utectulancingo.edu.mx>
2019-03-23 14:23:57 -0600

    0.- Se agregan las librerías necesarias para la conexion de iinternet con HTTP 1.- Se agregan librerias para tomar datos y guardarloe en un buffer 2.- Se crean variables lv_contacts_list para tener acceso a la lista que se creo en el Layout.
    3.- Se crea la variable adapter que es un ArrayAdapter para de almacenar los registros.
    4.- crea la variable getAllContactsURL que contiene la dirección URL a la que se realizará la conexión,
    
    5.- Se crea metodos para la conexion de servicios web
    6.- Se crea el metodo para hacer el parseo del archivo Json

--- Webservice/app/src/main/java/com/example/webservice/MainActivity.java
+++ Webservice/app/src/main/java/com/example/webservice/MainActivity.java
@@ -2,12 +2,77 @@ package com.example.webservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        lv_contacts_list = (ListView)findViewById(R.id.lv_contacts_list);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        lv_contacts_list.setAdapter(adapter);
+    }
+    private ListView lv_contacts_list;
+    private ArrayAdapter adapter;
+    private String getAllContactsURL = "http://192.168.0.23/agenda_rest/webservices/getAllContacts.php";
+
+
+    private void webServiceRest(String requestURL){
+        try{
+            URL url = new URL(requestURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = "";
+            String webServiceResult="";
+            while ((line = bufferedReader.readLine()) != null){
+                webServiceResult += line;
+            }
+            bufferedReader.close();
+            parseInformation(webServiceResult);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void parseInformation(String jsonResult){
+        JSONArray jsonArray = null;
+        String id;
+        String nombre;
+        String ape_pat;
+        String ape_mat;
+        String telefono;
+        String correo;
+        try{
+            jsonArray = new JSONArray(jsonResult);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        for(int i=0;i<jsonArray.length();i++){
+            try{
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                id = jsonObject.getString("id");
+                nombre = jsonObject.getString("nombre");
+                ape_pat = jsonObject.getString("ape_pat");
+                ape_mat = jsonObject.getString("ape_mat");
+                telefono = jsonObject.getString("telefono");
+                correo = jsonObject.getString("correo");
+                adapter.add(id + ": " + nombre);
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
